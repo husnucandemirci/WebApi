256839f8f2e0f67cc901656ef9d01d91102c5ea0
Hüsnücan Demirci <hsn__1@hotmail.com>
2015-12-23 01:54:25 +0200

    Model ve Controller'lar Eklendi. ( 2. Commit)
    Model ve Controller'lar eklendi.

---
+++ DamacanaWebApi/DamacanaWebApi/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DamacanaWebApi.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
+        public List<Product> Cartproducts { get; set; }
+
+     }
+}
---
+++ DamacanaWebApi/DamacanaWebApi/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DamacanaWebApi.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public List<Product> PurchaseList { get; set; }
+
+    }
+}
